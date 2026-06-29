c2d514a31894688a864c02908b26b6470b754b00
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2026-06-29 22:28:30 +0530

    feat: configure express app to use postgres adapter and register router paths

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,31 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import errorHandler from './middlewares/errorHandler.js';
 import { PrismaClient } from '@prisma/client';
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import authRoutes from './routes/authRoutes.js';
+import projectRoutes from './routes/projectRoutes.js';
 
 dotenv.config();
 
 const app = express();
-export const prisma = new PrismaClient();
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
 
 // Middlewares
 app.use(cors());
 app.use(express.json()); // JSON body parsing
 app.use(morgan('dev')); // Request logger
+
+// Serve static upload files
+app.use('/uploads', express.static('uploads'));
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/projects', projectRoutes);
+
 
 // Health check route
 app.get('/health', async (req, res, next) => {
